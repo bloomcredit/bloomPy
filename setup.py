982febdc97c5de8ab71df944aa462e6b5c38b142
GR <glenn@caffeinelab.com>
2024-05-14 16:05:19 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='bloomPy',
+    version='0.1.0',
+    packages=[''],
+    url='',
+    license='MIT License',
+    author='Glenn',
+    author_email='glenn@caffeinelab.com',
+    description='Use the Bloom Credit API with Python.'
+)
