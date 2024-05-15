d44b6215ed5d0e3a3df2320269e8cd593c1b7499
GR <glenn@caffeinelab.com>
2024-05-14 22:23:40 -0700

    add response to return for debugging ability

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '0.1.5'
+VERSION = '0.1.6'
 PACKAGE_NAME = 'bloomcredit-python'
 AUTHOR = 'Glenn'
 AUTHOR_EMAIL = 'glenn@caffeinelab.com'
