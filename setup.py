f8ddbeabdfb03e3aadff5bca4c4b87cc17bca850
GR <glenn@caffeinelab.com>
2024-05-14 20:29:48 -0700

    update package manifest

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '0.1.2'
+VERSION = '0.1.4'
 PACKAGE_NAME = 'bloomPy'
 AUTHOR = 'Glenn'
 AUTHOR_EMAIL = 'glenn@caffeinelab.com'
