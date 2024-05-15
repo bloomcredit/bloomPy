9511bf9ef5f467c4af791228d167ac1fe6708ccb
GR <glenn@caffeinelab.com>
2024-05-14 17:49:25 -0700

    add setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,34 @@
-from setuptools import setup
+import pathlib
+from setuptools import setup, find_packages
 
-setup(
-    name='bloomPy',
-    version='0.1.0',
-    packages=['bloom'],
-    url='',
-    license='MIT License',
-    author='Glenn',
-    author_email='glenn@caffeinelab.com',
-    description='Use the Bloom Credit API with Python.'
-)
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.1.0'
+PACKAGE_NAME = 'bloomPy'
+AUTHOR = 'Glenn'
+AUTHOR_EMAIL = 'glenn@caffeinelab.com'
+URL = ''
+LICENSE = 'MIT License'
+DESCRIPTION = 'Use the Bloom Credit API with Python'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+      'python-dotenv',
+      'requests'
+]
+
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages(where="src"),
+      package_dir={"": "src"},
+      )
